dbc86357ea9edea70eebcbbf27df7bba50ee05a7
Unknown <lourido2003@yahoo.es>
2016-11-14 21:56:32 +0100

    Parcial 1 del ejercicio

--- 01_temperaturas_dia/01_temperaturas_dia/src/com/lourido/Main.java
+++ 01_temperaturas_dia/01_temperaturas_dia/src/com/lourido/Main.java
@@ -1,8 +1,59 @@
 package com.lourido;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int horas = 24;
+        double[] temperatura;
+        // inicializo el array a 99.0
+        temperatura = new double[horas];
+        for (int i = 0; i < temperatura.length; i++) {
+            System.out.println("Dame las temperaturas durante el día");
+            temperatura[i] = Double.parseDouble(br.readLine());
+        }
+
+        // calcular la media, el máximo y el mínimo
+        double total = 0;
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
+
+        for (int i = 0; i < temperatura.length; i++) {
+            total += temperatura[i];
+            if (temperatura[i] > max) {
+                max = temperatura[i];
+            }
+            if (temperatura[i] < min) {
+                min = temperatura[i];
+            }
+            double media = total / temperatura.length;
+
+            // hacer el dibujo
+
+            for ( i = 0; i < temperatura.length; i++) {
+                System.out.print(i);
+
+                System.out.print(" ");
+
+                for (int j = 0; j < temperatura[i]; j++) {
+                    System.out.print("*");
+                }
+                System.out.print(" " + temperatura[i]);
+
+                if (temperatura[i]== max){
+                    System.out.print(" --> MAX");
+                }
+                if (temperatura[i]== min){
+                    System.out.print(" --> MIN");
+                }
+                System.out.println();
+            }
+
+        }
+
     }
 }
